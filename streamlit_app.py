eda8a00f27050cd53d4950e03f702804e6f66c6f
Devendra Khatri <116125046+unclekhatru@users.noreply.github.com>
2022-10-20 10:33:36 -0700

    Update streamlit_app.py
    watermelon to kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruits api response
 streamlit.header('Fruityvice Fruit Advice!')
 import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json()) # just writes data to the screen
 
 
