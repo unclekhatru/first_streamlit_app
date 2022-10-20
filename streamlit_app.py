0b63cfca7432fe08f6430760ab887966abebb921
Devendra Khatri <116125046+unclekhatru@users.noreply.github.com>
2022-10-20 13:47:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,12 @@ import snowflake.connector
 from urllib.error import URLError 
 
 streamlit.title('My Mom New Healthy Diner') 
-
 streamlit.header('Breakfast Favorites') 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal') 
 streamlit.text('🥗 Kale, Spinach & Rocket Smotthie') 
 streamlit.text('🐔 Hard Boiled Free-Range Egg') 
 streamlit.text('🥑🍞 Avacado Toast') 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -21,24 +18,37 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected =streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries','Apple'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
+
+
 # New Section to display fruits api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('what fruit would you like information about?','kiwi')
+try:
+  fruit_choice = streamlit.text_input('what fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:   
+      fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+    
+      
 streamlit.write('The user entered',fruit_choice)
 
 #import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) # just writes data to the screen
 
 
 #take the json version of the response and nomalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output it the screen as table
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 #dont run anything past here while we troubleshoot
 streamlit.stop()
