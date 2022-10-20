ebebef5ea300ef6af1ca82339dc23c1fe78332c4
Devendra Khatri <116125046+unclekhatru@users.noreply.github.com>
2022-10-20 12:39:15 -0700

    Update streamlit_app.py
    39 to 44

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output it the screen as table
 streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector 
+
+my_cnx = snowflake.connector.conect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("Select CURRENT_USER(),CURRENT_ACCOUNT() , CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.tetx("Hellow from Snowflake:")
+streamlit.tetx(my_data_row)
